649c26fbdcaf05793bec55bdafb9eb6855e2e935
Cherono-mercy <cheronom142@gmail.com>
2024-02-18 18:09:31 +0800

    completed

--- server/app.py
+++ server/app.py
@@ -18,6 +18,42 @@ db.init_app(app)
 
 api = Api(app)
 
+# Login resource
+class Login(Resource):
+    # Has one route, post()
+    def post(self):
+        # post() gets a username from request's JSON and retrieves the user by username
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+        # post() sets the session's 'user_id' to the user's id
+        session['user_id'] = user.id
+        # post() returns the user as JSON with a 200 status code
+        return user.to_dict(), 200
+
+# Logout resource    
+class Logout(Resource):
+    # Has one route, delete()
+    def delete(self):
+        # delete() removes user_id value from the session
+        session['user_id'] = None
+        # delete() returns no data and a 204(No content) status code
+        return {}, 204
+
+# CheckSession resource located at '/check_session'
+class CheckSession(Resource):
+    # Has one route, get()
+    def get(self):
+        # get() retrieves the user_id value from the session
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        # If the session has a user_id, get() returns the user as JSON with a 200 status code
+        if user:
+            return user.to_dict(), 200
+        # If the session does not have a user_id, get() returns no data and a 401 (Unauthorized) status code
+        else:
+            return {}, 401
+
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -51,6 +87,9 @@ class ShowArticle(Resource):
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
